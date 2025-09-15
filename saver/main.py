4319b13ef2cec0ee91e4d523d87d1113ba19b794
girvel <widauka@ya.ru>
2025-09-15 14:35:37 +0500

    Make saver stable & compatible with any topic format

--- saver/main.py
+++ saver/main.py
@@ -5,17 +5,22 @@ import logging
 from datetime import datetime
 
 def on_message(client, userdata, message):
-    conn = userdata
-    print(f"{message.topic}: {message.payload.decode()}")
-    if message.topic.startswith("home/"):
-        _, sensor, kind = message.topic.split("/")
-        assert kind in ("temperature", "humidity")
-        value = float(message.payload.decode())
-        conn.execute(
-            f"INSERT INTO {kind} (sensor, value, timestamp) VALUES (%s, %s, %s)",
-            (sensor, value, datetime.now())
-        )
-        conn.commit()
+    try:
+        conn = userdata
+        print(f"{message.topic}: {message.payload.decode()}")
+        if not message.topic.startswith("home/"): return
+        segments = tuple(message.topic.split("/")[1:])
+
+        match segments:
+            case (sensor, kind) if kind in ("humidity", "temperature"):
+                value = float(message.payload.decode())
+                conn.execute(
+                    f"INSERT INTO {kind} (sensor, value, timestamp) VALUES (%s, %s, %s)",
+                    (sensor, value, datetime.now())
+                )
+                conn.commit()
+    except Exception as ex:
+        print(ex)
 
 postgres_host = os.getenv("POSTGRES_HOST")
 assert postgres_host is not None
