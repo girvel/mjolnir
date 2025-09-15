b5ba683e7489a07033da7e52897b2119ea8be6e3
girvel <widauka@ya.ru>
2025-09-15 14:44:46 +0500

    homepage: A bit better docs for index page

--- homepage/main.go
+++ homepage/main.go
@@ -8,15 +8,15 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-// @title thor1 homepage
-// @description Website with information about the home server
+// @title  thor1 homepage
+// @description  Website with information about the home server
 
-// @host thor1
+// @host  thor1
 
-// @Summary Homepage
-// @Description Provides an overview of the server
-// @Produce html
-// @Router / [get]
+// @Summary  Homepage
+// @Description  Provides an overview of the server, contains all general information about its capabilities and local network
+// @Produce  html
+// @Router  / [get]
 func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H {})
 }
