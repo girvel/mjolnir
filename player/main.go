d98510004b912515ca599fd8abcbbbbd2da7974f
girvel <widauka@ya.ru>
2025-09-20 22:33:58 +0500

    player: iPhone player compatibility

--- player/main.go
+++ player/main.go
@@ -5,15 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// @title  thor1 homepage
-// @description  Website with information about the home server
-
-// @host  thor1
-
-// @Summary  Homepage
-// @Description  Provides an overview of the server, contains all general information about its capabilities and local network
-// @Produce  html
-// @Router  / [get]
 func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H {})
 }
