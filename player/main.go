98ae793643e989207a8c5f1cccbd22537a40a9b1
girvel <widauka@ya.ru>
2025-09-21 16:34:29 +0500

    player: Playlist (broke iPhone controls)

--- player/main.go
+++ player/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"os"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
 func index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl", gin.H {})
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+}
+
+const audioPrefix = "static/audio/"
+
+func playlist(c *gin.Context) {
+	entries, err := os.ReadDir(audioPrefix)
+	if err != nil {
+	    c.JSON(http.StatusInternalServerError, gin.H{"message": "unable to read audio directory"})
+		return
+	}
+
+	var items []string
+	for _, e := range entries {
+		name := e.Name()
+		if name[0] == '.' { continue }
+	    items = append(items, name)
+	}
+
+    c.JSON(http.StatusOK, gin.H{
+        "items": items,
+		"prefix": audioPrefix,
+    })
 }
 
 func main() {
@@ -15,6 +38,7 @@ func main() {
 	router.Static("/static", "./static")
 
 	router.GET("/", index)
+	router.GET("/api/playlist", playlist)
 
 	router.Run()
 }
