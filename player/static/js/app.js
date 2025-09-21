4960b037dc365796262836b7215e0852e4004db8
girvel <widauka@ya.ru>
2025-09-22 01:22:33 +0500

    player: Tracklist

--- player/static/js/app.js
+++ player/static/js/app.js
@@ -9,12 +9,13 @@ const mod = (a, b) => {
 }
 
 const player = {
-  dom_element: document.getElementById("player"),
+  audio_dom: document.getElementById("player"),
+  tracklist_dom: document.getElementById("tracklist"),
   playlist: undefined,
   playlistIndex: -1,
 
   init: function() {
-    this.dom_element.addEventListener("ended", () => this.shiftTrack(1));
+    this.audio_dom.addEventListener("ended", () => this.shiftTrack(1));
     if ('mediaSession' in navigator) {
       navigator.mediaSession.setActionHandler("nexttrack", () => this.shiftTrack(1));
       navigator.mediaSession.setActionHandler("previoustrack", () => this.shiftTrack(-1));
@@ -28,10 +29,18 @@ const player = {
 
   shiftTrack: function(offset) {
     this.playlistIndex = mod(this.playlistIndex + offset, this.playlist.items.length);
+
+    let tracklist_render = "";
+    for (const [i, filename] of this.playlist.items.entries()) {
+      if (i > 0) tracklist_render += "\n";
+      tracklist_render += (i == this.playlistIndex ? ">" : "-") + " " + filename;
+    }
+    this.tracklist_dom.textContent = tracklist_render;
+
     const filename = this.playlist.items[this.playlistIndex];
 
-    this.dom_element.src = this.playlist.prefix + filename;
-    this.dom_element.play();
+    this.audio_dom.src = this.playlist.prefix + filename;
+    this.audio_dom.play();
 
     if ('mediaSession' in navigator) {
       const [artist, title] = removeExtension(filename).split(" - ", 2);
