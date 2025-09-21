53f7b6e9c46baf9a61ecd42f1e57b8f410606ef6
girvel <widauka@ya.ru>
2025-09-22 01:14:37 +0500

    player: Pack logic into an object

--- player/static/js/app.js
+++ player/static/js/app.js
@@ -8,33 +8,39 @@ const mod = (a, b) => {
   return ((a % b) + b) % b;
 }
 
-const player = document.getElementById("player");
-let playlist;
+const player = {
+  dom_element: document.getElementById("player"),
+  playlist: undefined,
+  playlistIndex: -1,
 
-let playlistIndex = -1;
-const shiftTrack = (offset) => {
-  playlistIndex = mod(playlistIndex + offset, playlist.items.length);
-  const filename = playlist.items[playlistIndex];
+  init: function() {
+    this.dom_element.addEventListener("ended", () => this.shiftTrack(1));
+    if ('mediaSession' in navigator) {
+      navigator.mediaSession.setActionHandler("nexttrack", () => this.shiftTrack(1));
+      navigator.mediaSession.setActionHandler("previoustrack", () => this.shiftTrack(-1));
+    }
 
-  player.src = playlist.prefix + filename;
-  player.play();
-
-  if ('mediaSession' in navigator) {
-    const [artist, title] = removeExtension(filename).split(" - ", 2);
-    navigator.mediaSession.metadata = new MediaMetadata({
-      artist: artist,
-      title: title,
+    axios.get("/api/playlist").then((response) => {
+      this.playlist = response.data;
+      this.shiftTrack(1);
     });
-  }
-};
+  },
 
-player.addEventListener("ended", () => shiftTrack(1));
-if ('mediaSession' in navigator) {
-  navigator.mediaSession.setActionHandler("nexttrack", () => shiftTrack(1));
-  navigator.mediaSession.setActionHandler("previoustrack", () => shiftTrack(-1));
+  shiftTrack: function(offset) {
+    this.playlistIndex = mod(this.playlistIndex + offset, this.playlist.items.length);
+    const filename = this.playlist.items[this.playlistIndex];
+
+    this.dom_element.src = this.playlist.prefix + filename;
+    this.dom_element.play();
+
+    if ('mediaSession' in navigator) {
+      const [artist, title] = removeExtension(filename).split(" - ", 2);
+      navigator.mediaSession.metadata = new MediaMetadata({
+        artist: artist,
+        title: title,
+      });
+    }
+  },
 }
 
-axios.get("/api/playlist").then((response) => {
-  playlist = response.data;
-  shiftTrack(1);
-});
+player.init();
