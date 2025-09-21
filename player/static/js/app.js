0b626a15afc85c5a92538f1ac7fd21a16e9d9504
girvel <widauka@ya.ru>
2025-09-22 00:57:19 +0500

    player: Reformat app.js, show title & name

--- player/static/js/app.js
+++ player/static/js/app.js
@@ -1,27 +1,34 @@
+const removeExtension = (filename) => {
+  let index = filename.lastIndexOf(".");
+  if (index == -1) return filename;
+  return filename.substring(0, index);
+}
+
 const player = document.getElementById("player");
 let playlist;
 
 let playlistIndex = -1;
 const playNextTrack = () => {
-    playlistIndex++;
-    const name = playlist.items[playlistIndex];
-    player.src = playlist.prefix + name;
-    player.autoplay = true;
+  playlistIndex++;
+  const filename = playlist.items[playlistIndex];
+  player.src = playlist.prefix + filename;
+  player.autoplay = true;
 
-    if ('mediaSession' in navigator) {
-        navigator.mediaSession.metadata = new MediaMetadata({
-            title: name,
-            //artist: "Vibe Chemistry",
-        });
-    }
+  if ('mediaSession' in navigator) {
+    const [artist, title] = removeExtension(filename).split(" - ", 2);
+    navigator.mediaSession.metadata = new MediaMetadata({
+      artist: artist,
+      title: title,
+    });
+  }
 };
 
 player.addEventListener("ended", playNextTrack);
 if ('mediaSession' in navigator) {
-    navigator.mediaSession.setActionHandler("nexttrack", () => playNextTrack());
+  navigator.mediaSession.setActionHandler("nexttrack", () => playNextTrack());
 }
 
 axios.get("/api/playlist").then((response) => {
-    playlist = response.data;
-    playNextTrack();
+  playlist = response.data;
+  playNextTrack();
 });
