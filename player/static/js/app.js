c627b920c20bc8bc9a0b9f257ffeb60cbd8388a7
girvel <widauka@ya.ru>
2025-09-22 01:07:28 +0500

    player: Switch to previous track

--- player/static/js/app.js
+++ player/static/js/app.js
@@ -4,15 +4,20 @@ const removeExtension = (filename) => {
   return filename.substring(0, index);
 }
 
+const mod = (a, b) => {
+  return ((a % b) + b) % b;
+}
+
 const player = document.getElementById("player");
 let playlist;
 
 let playlistIndex = -1;
-const playNextTrack = () => {
-  playlistIndex++;
+const shiftTrack = (offset) => {
+  playlistIndex = mod(playlistIndex + offset, playlist.items.length);
   const filename = playlist.items[playlistIndex];
+
   player.src = playlist.prefix + filename;
-  player.autoplay = true;
+  player.play();
 
   if ('mediaSession' in navigator) {
     const [artist, title] = removeExtension(filename).split(" - ", 2);
@@ -23,12 +28,13 @@ const playNextTrack = () => {
   }
 };
 
-player.addEventListener("ended", playNextTrack);
+player.addEventListener("ended", () => shiftTrack(1));
 if ('mediaSession' in navigator) {
-  navigator.mediaSession.setActionHandler("nexttrack", () => playNextTrack());
+  navigator.mediaSession.setActionHandler("nexttrack", () => shiftTrack(1));
+  navigator.mediaSession.setActionHandler("previoustrack", () => shiftTrack(-1));
 }
 
 axios.get("/api/playlist").then((response) => {
   playlist = response.data;
-  playNextTrack();
+  shiftTrack(1);
 });
